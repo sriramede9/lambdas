166a8c815327a44d3d7a9cd9b582132768b5243b
sriramede9 <only4programming03@gmail.com>
2019-05-03 02:32:19 -0400

    Sending behavior as an arguement

--- Lambda expressions/src/Introduction/Test1.java
+++ Lambda expressions/src/Introduction/Test1.java
@@ -1,11 +1,15 @@
 package Introduction;
 
 public class Test1 {
+	
+	static void meth(Shape s) {
+		s.draw();
+	}
+	
 	public static void main(String[] args) {
 		Shape rectangle=()-> System.out.println("this is rectangle calling");
 		rectangle.draw();
 		
-		String s="Sriramede";
 		
 		Shape1 circlearea=(a,b)-> 2*a*b;
 		
@@ -21,8 +25,14 @@ public class Test1 {
 		
 		Thread t1=new Thread(r);
 		Thread t2=new Thread(r);
-		t1.start();
-		t2.start();
+//		t1.start();
+//		t2.start();
+		
+		Shape square=()->System.out.println("Square here");
+		
+		meth(square); // sending behavior as an arguement.
+		
+		meth(()->System.out.println("this is another symbol"));
 		
 	}
 }
